00870b18a16aef405493b2c82cbd2b44c2509100
Max Van den Eynde <maxvdec@gmail.com>
2026-09-20 12:55:36 +0200

    Refined the test suite

--- tests/runner.cpp
+++ tests/runner.cpp
@@ -7,6 +7,10 @@
 #include <tuple>
 #include <vector>
 
+std::string getTestBuildOutputPath(const std::string &fileName) {
+    return std::string(TESTS_PATH) + "/build/" + fileName;
+}
+
 std::tuple<bool, std::string> runCommand(const std::string &command) {
     std::array<char, 256> buffer{};
     std::string result;
@@ -41,13 +45,15 @@ std::string readFile(const std::string &path) {
 
 void runExecutableSuite() {
     // Compile sources
-    std::string compileCommand = "powerpc-eabi-as -mbroadway " +
-                                 std::string(TESTS_PATH) +
-                                 "/core/executable.s -o /tmp/executable.o";
-    std::string linkCommand = "powerpc-eabi-ld -Ttext=0x80004000 -e _start "
-                              "/tmp/executable.o -o /tmp/executable.elf";
-    std::string toDolfCommand =
-        "elf2dol /tmp/executable.elf /tmp/executable.dol";
+    std::string compileCommand =
+        "powerpc-eabi-as -mbroadway " + std::string(TESTS_PATH) +
+        "/core/executable.s -o " + getTestBuildOutputPath("executable.o");
+    std::string linkCommand = "powerpc-eabi-ld -Ttext=0x80004000 -e _start " +
+                              getTestBuildOutputPath("executable.o") + " -o " +
+                              getTestBuildOutputPath("executable.elf");
+    std::string toDolfCommand = "elf2dol " +
+                                getTestBuildOutputPath("executable.elf") + " " +
+                                getTestBuildOutputPath("executable.dol");
 
     auto [compileSuccess, compileOutput] = runCommand(compileCommand);
     if (!compileSuccess) {
@@ -62,14 +68,16 @@ void runExecutableSuite() {
         throw std::runtime_error("elf2dol failed: " + toDolfOutput);
     }
     auto [runDolSuccess, runDolOutput] =
-        runCommand(std::string(REVOLVE_PATH) + " parse /tmp/executable.dol");
+        runCommand(std::string(REVOLVE_PATH) + " parse " +
+                   getTestBuildOutputPath("executable.dol"));
     if (!runDolSuccess) {
         throw std::runtime_error("Dolphin execution failed: " + runDolOutput);
     }
     std::cout << runDolOutput << std::endl;
     std::cout << "-------------" << std::endl;
     auto [runElfSuccess, runElfOutput] =
-        runCommand(std::string(REVOLVE_PATH) + " parse /tmp/executable.elf");
+        runCommand(std::string(REVOLVE_PATH) + " parse " +
+                   getTestBuildOutputPath("executable.elf"));
     if (!runElfSuccess) {
         throw std::runtime_error("Dolphin execution failed: " + runElfOutput);
     }
@@ -85,7 +93,19 @@ int main(int argc, char *argv[]) {
                                     "test suite name as an argument.");
     }
 
+    std::string cleanCommand = "mkdir -p " + std::string(TESTS_PATH) + "/build";
+    runCommand(cleanCommand);
+
     if (suite == "executable") {
         runExecutableSuite();
+    }
+
+    if (suite == "clean") {
+        std::string cleanCommand =
+            "rm -rf " + std::string(TESTS_PATH) + "/build";
+        auto [cleanSuccess, cleanOutput] = runCommand(cleanCommand);
+        if (!cleanSuccess) {
+            throw std::runtime_error("Clean failed: " + cleanOutput);
+        }
     }
 }
