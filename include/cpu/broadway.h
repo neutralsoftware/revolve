820e05a24ea81e304eeceab7e1f30a3641659f09
Max Van den Eynde <maxvdec@gmail.com>
2026-09-20 20:36:34 +0200

    Begun implementation of instruction parsing and execution

--- include/cpu/broadway.h
+++ include/cpu/broadway.h
@@ -5,50 +5,203 @@
 #include "core/utils.h"
 #include <cstdint>
 
+static constexpr uint32_t XER_SO = 1u << 31;
+static constexpr uint32_t XER_OV = 1u << 30;
+static constexpr uint32_t XER_CA = 1u << 29;
+
+static constexpr uint32_t XER_BC_MASK = 0x7Fu;
+
+enum SPR : uint16_t {
+    XER = 1,
+    LR = 8,
+    CTR = 9,
+
+    DSISR = 18,
+    DAR = 19,
+    DEC = 22,
+
+    SDR1 = 25,
+    SRR0 = 26,
+    SRR1 = 27,
+
+    SPRG0 = 272,
+    SPRG1 = 273,
+    SPRG2 = 274,
+    SPRG3 = 275,
+
+    PVR = 287,
+
+    GQR0 = 912,
+    GQR1 = 913,
+    GQR2 = 914,
+    GQR3 = 915,
+    GQR4 = 916,
+    GQR5 = 917,
+    GQR6 = 918,
+    GQR7 = 919,
+
+    HID2 = 920,
+    WPAR = 921,
+
+    HID0 = 1008,
+    HID1 = 1009,
+    HID4 = 1011,
+    DABR = 1013,
+    L2CR = 1017
+};
+
+enum class BroadwayDTypeInstruction : uint32_t {
+    // D-Type Instructions
+    TWI = 3,
+    MULLI = 7,
+    SUBFIC = 8,
+    CMPLI = 10,
+    CMPI = 11,
+    ADDIC = 12,
+    ADDIC_ = 13,
+    ADDI = 14,
+    ADDIS = 15,
+    ORI = 24,
+    ORIS = 25,
+    XORI = 26,
+    XORIS = 27,
+    ANDI_ = 28,
+    ANDIS_ = 29,
+    LWZ = 32,
+    LWZU = 33,
+    LBZ = 34,
+    LBZU = 35,
+    STW = 36,
+    STWU = 37,
+    STB = 38,
+    STBU = 39,
+    LHZ = 40,
+    LHZU = 41,
+    LHA = 42,
+    LHAU = 43,
+    STH = 44,
+    STHU = 45,
+    LMW = 46,
+    STMW = 47,
+    LFS = 48,
+    LFSU = 49,
+    LFD = 50,
+    LFDU = 51,
+    STFS = 52,
+    STFSU = 53,
+    STFD = 54,
+    STFDU = 55,
+};
+
+enum class InstructionType {
+    D,
+    I,
+    B,
+    SC,
+    X,
+    XO,
+    XL,
+    XFX,
+    XFL,
+    M,
+    A,
+    PSQ_D,
+    PSQ_X
+};
+
 struct BroadwayState : public Loggable {
     uint32_t gpr[32]{}; // General Purpose Registers
 
     uint64_t fpr[32]{}; // Floating Point Registers
 
-    uint32_t pc = 0; // Program Counter
+    uint32_t cr{};    // Condition Register
+    uint32_t fpscr{}; // Floating Point Status and Control Register
+    uint32_t msr{};   // Machine State Register
 
-    uint32_t cr = 0;  // Condition Register
-    uint32_t xer = 0; // Fixed-Point Exception Register
-    uint32_t lr = 0;  // Link Register
-    uint32_t ctr = 0; // Count Register
+    uint32_t sr[16]{}; // Segment Registers
 
-    uint32_t fpscr = 0; // Floating Point Status and Control Register
+    uint32_t spr[1024]{}; // Special Purpose Registers
 
-    uint32_t msr = 0; // Machine State Register
+    uint32_t cia{}; // Current Instruction Address
+    uint32_t nia{}; // Next Instruction Address
 
-    uint32_t srr0 = 0;  // Save/Restore Register 0
-    uint32_t srr1 = 0;  // Save/Restore Register 1
-    uint32_t dar = 0;   // Data Address Register
-    uint32_t dsisr = 0; // Data Storage Interrupt Status Register
-
-    uint32_t dec = 0;      // Decrementer
-    uint32_t timeBase = 0; // Time Base
-
-    uint32_t sprg[4]{}; // Special Purpose Registers General
-    uint32_t sr[16]{};  // Segment Registers
-    uint32_t sdr1 = 0;  // Storage Description Register 1
-
-    // Broadway / Gekko specific registers
-    uint32_t gqr[8]{}; // Graphics Quantization Registers
-
-    uint32_t hid0 = 0; // Hardware Implementation-Dependent Register 0
-    uint32_t hid1 = 0; // Hardware Implementation-Dependent Register 1
-    uint32_t hid2 = 0; // Hardware Implementation-Dependent Register 2
-    uint32_t hid4 = 0; // Hardware Implementation-Dependent Register 4
-
-    uint32_t wpar = 0; // Watchpoint Address Register
-    uint32_t l2cr = 0; // L2 Cache Control Register
-
-    uint32_t pvr = 0; // Processor Version Register
+    bool reservationValid{};       // Reservation Valid Bit
+    uint32_t reservationAddress{}; // Reservation Address
 
     std::string log() const override;
     std::string getLogSystem() const override { return "Broadway"; }
+
+    inline uint32_t &getSPR(SPR sprIndex) {
+        return spr[static_cast<uint16_t>(sprIndex)];
+    }
+
+    inline bool getSO() {
+        return (spr[static_cast<uint16_t>(SPR::XER)] & XER_SO) != 0;
+    }
+
+    inline bool getOV() {
+        return (spr[static_cast<uint16_t>(SPR::XER)] & XER_OV) != 0;
+    }
+
+    inline bool getCA() {
+        return (spr[static_cast<uint16_t>(SPR::XER)] & XER_CA) != 0;
+    }
+
+    inline void setSO(bool value) {
+        if (value) {
+            spr[static_cast<uint16_t>(SPR::XER)] |= XER_SO;
+        } else {
+            spr[static_cast<uint16_t>(SPR::XER)] &= ~XER_SO;
+        }
+    }
+
+    inline void setOV(bool value) {
+        if (value) {
+            spr[static_cast<uint16_t>(SPR::XER)] |= XER_OV;
+        } else {
+            spr[static_cast<uint16_t>(SPR::XER)] &= ~XER_OV;
+        }
+    }
+
+    inline void setCA(bool value) {
+        if (value) {
+            spr[static_cast<uint16_t>(SPR::XER)] |= XER_CA;
+        } else {
+            spr[static_cast<uint16_t>(SPR::XER)] &= ~XER_CA;
+        }
+    }
+
+    inline void setCRField(uint32_t fieldIndex, uint32_t value) {
+        uint32_t mask = 0b1111u << (28 - fieldIndex * 4);
+        cr = (cr & ~mask) | ((value & 0b1111u) << (28 - fieldIndex * 4));
+    }
+
+    inline uint32_t getCRField(uint32_t fieldIndex) {
+        return (cr >> (28 - fieldIndex * 4)) & 0b1111u;
+    }
+
+    inline void updateCR0(uint32_t in) {
+        int32_t result = static_cast<int32_t>(in);
+        uint32_t cr0 = 0;
+
+        if (result < 0)
+            cr0 |= 0b1000;
+        else if (result > 0)
+            cr0 |= 0b0100;
+        else
+            cr0 |= 0b0010;
+
+        if (getSO())
+            cr0 |= 0b0001;
+
+        setCRField(0, cr0);
+    }
 };
+
+constexpr int32_t signExtend(uint32_t x, unsigned bits) {
+    const uint32_t sign = 1u << (bits - 1);
+    return static_cast<int32_t>((x ^ sign) - sign);
+}
 
 class Broadway {
   public:
@@ -57,7 +210,67 @@ class Broadway {
     void reset(uint32_t entryPoint);
     void executeInstruction();
 
+    void executeDType(uint32_t instruction);
+    void executeIType(uint32_t instruction);
+    void executeBType(uint32_t instruction);
+    void executeSCType(uint32_t instruction);
+    void executeXType(uint32_t instruction);
+    void executeXOType(uint32_t instruction);
+    void executeXLType(uint32_t instruction);
+    void executeXFXType(uint32_t instruction);
+    void executeXFLType(uint32_t instruction);
+    void executeMType(uint32_t instruction);
+    void executeAType(uint32_t instruction);
+    void executePSQ_DType(uint32_t instruction);
+    void executePSQ_XType(uint32_t instruction);
+
+    InstructionType getInstructionType(uint32_t instruction);
+
     void start();
+
+    // D Type instructions
+    void executeTWI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeMULLI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSUBFIC(uint32_t field0, uint32_t field1, uint32_t,
+                       int32_t simm);
+    void executeCMPLI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeCMPI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeADDIC(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeADDIC_(uint32_t field0, uint32_t field1, uint32_t,
+                       int32_t simm);
+    void executeADDI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeADDIS(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeORI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeORIS(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeXORI(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeXORIS(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeANDI_(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeANDIS_(uint32_t field0, uint32_t field1, uint32_t,
+                       int32_t simm);
+    void executeLWZ(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLWZU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLBZ(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLBZU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTW(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTWU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTB(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTBU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLHZ(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLHZU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLHA(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLHAU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTH(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTHU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLMW(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTMW(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLFS(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLFSU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLFD(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeLFDU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTFS(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTFSU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTFD(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
+    void executeSTFDU(uint32_t field0, uint32_t field1, uint32_t, int32_t simm);
 
   private:
     MemoryStream instructionStream{0};
--- src/cpu/broadway.cpp
+++ src/cpu/broadway.cpp
@@ -1,42 +1,32 @@
 #include "cpu/broadway.h"
+#include "core/utils.h"
 #include <cstdint>
 #include <string>
 
 void Broadway::reset(uint32_t entryPoint) {
     state = {};
-    state.pc = entryPoint;
+    state.cia = entryPoint;
 
     instructionStream = MemoryStream(entryPoint);
 }
 
 std::string BroadwayState::log() const {
     std::string logMessage = "Broadway State:\n";
-    logMessage += "PC: 0x" + std::to_string(pc) + "\n";
-    logMessage += "MSR: 0x" + std::to_string(msr) + "\n";
-    logMessage += "CR: 0x" + std::to_string(cr) + "\n";
-    logMessage += "XER: 0x" + std::to_string(xer) + "\n";
-    logMessage += "LR: 0x" + std::to_string(lr) + "\n";
-    logMessage += "CTR: 0x" + std::to_string(ctr) + "\n";
-    logMessage += "FPSCR: 0x" + std::to_string(fpscr) + "\n";
-    logMessage += "SRR0: 0x" + std::to_string(srr0) + "\n";
-    logMessage += "SRR1: 0x" + std::to_string(srr1) + "\n";
-    logMessage += "DAR: 0x" + std::to_string(dar) + "\n";
-    logMessage += "DSISR: 0x" + std::to_string(dsisr) + "\n";
-    logMessage += "DEC: 0x" + std::to_string(dec) + "\n";
-    logMessage += "Time Base: 0x" + std::to_string(timeBase) + "\n";
-
+    logMessage += "CIA: 0x" + utils::toHexString(cia) + "\n";
+    logMessage += "NIA: 0x" + utils::toHexString(nia) + "\n";
+    logMessage += "MSR: 0x" + utils::toHexString(msr) + "\n";
+    logMessage += "CR: 0x" + utils::toHexString(cr) + "\n";
+    logMessage += "FPSCR: 0x" + utils::toHexString(fpscr) + "\n";
     logMessage += "GPRs:\n";
     for (int i = 0; i < 32; ++i) {
-        logMessage += "  GPR[" + std::to_string(i) + "]: 0x" +
-                      std::to_string(gpr[i]) + "\n";
+        logMessage += "  R" + std::to_string(i) + ": 0x" +
+                      utils::toHexString(gpr[i]) + "\n";
     }
-
     logMessage += "FPRs:\n";
     for (int i = 0; i < 32; ++i) {
-        logMessage += "  FPR[" + std::to_string(i) + "]: 0x" +
-                      std::to_string(fpr[i]) + "\n";
+        logMessage += "  F" + std::to_string(i) + ": 0x" +
+                      utils::toHexString(static_cast<uint32_t>(fpr[i])) + "\n";
     }
-
     return logMessage;
 }
 
@@ -46,6 +36,252 @@ void Broadway::start() {
     }
 }
 
+InstructionType Broadway::getInstructionType(uint32_t instruction) {
+    uint32_t op = instruction >> 26;
+    switch (static_cast<BroadwayDTypeInstruction>(op)) {
+    case BroadwayDTypeInstruction::TWI:
+    case BroadwayDTypeInstruction::MULLI:
+    case BroadwayDTypeInstruction::SUBFIC:
+    case BroadwayDTypeInstruction::CMPLI:
+    case BroadwayDTypeInstruction::CMPI:
+    case BroadwayDTypeInstruction::ADDIC:
+    case BroadwayDTypeInstruction::ADDIC_:
+    case BroadwayDTypeInstruction::ADDI:
+    case BroadwayDTypeInstruction::ADDIS:
+    case BroadwayDTypeInstruction::ORI:
+    case BroadwayDTypeInstruction::ORIS:
+    case BroadwayDTypeInstruction::XORI:
+    case BroadwayDTypeInstruction::XORIS:
+    case BroadwayDTypeInstruction::ANDI_:
+    case BroadwayDTypeInstruction::ANDIS_:
+    case BroadwayDTypeInstruction::LWZ:
+    case BroadwayDTypeInstruction::LWZU:
+    case BroadwayDTypeInstruction::STW:
+    case BroadwayDTypeInstruction::STWU:
+    case BroadwayDTypeInstruction::STB:
+    case BroadwayDTypeInstruction::STBU:
+    case BroadwayDTypeInstruction::LHZ:
+    case BroadwayDTypeInstruction::LHZU:
+    case BroadwayDTypeInstruction::LHA:
+    case BroadwayDTypeInstruction::LHAU:
+    case BroadwayDTypeInstruction::STH:
+    case BroadwayDTypeInstruction::STHU:
+    case BroadwayDTypeInstruction::LMW:
+    case BroadwayDTypeInstruction::STMW:
+    case BroadwayDTypeInstruction::LFS:
+    case BroadwayDTypeInstruction::LFSU:
+    case BroadwayDTypeInstruction::LFD:
+    case BroadwayDTypeInstruction::LFDU:
+    case BroadwayDTypeInstruction::STFS:
+    case BroadwayDTypeInstruction::STFSU:
+    case BroadwayDTypeInstruction::STFD:
+    case BroadwayDTypeInstruction::STFDU:
+    case BroadwayDTypeInstruction::LBZ:
+    case BroadwayDTypeInstruction::LBZU:
+        return InstructionType::D;
+    }
+
+    return InstructionType::I; // Default to I-Type for unrecognized opcodes
+}
+
+void Broadway::executeDType(uint32_t instruction) {
+    uint32_t op = instruction >> 26;
+
+    uint32_t field0 = (instruction >> 21) & 0x1F;
+    uint32_t field1 = (instruction >> 16) & 0x1F;
+
+    uint32_t uimm = instruction & 0xFFFF;
+    int32_t simm = signExtend(uimm, 16);
+
+    // Note some instructions represent A = 0 as "no register" and should be
+    // handled accordingly.
+    BroadwayDTypeInstruction dtypeInstruction =
+        static_cast<BroadwayDTypeInstruction>(op);
+    switch (dtypeInstruction) {
+    case BroadwayDTypeInstruction::TWI:
+        executeTWI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::MULLI:
+        executeMULLI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::SUBFIC:
+        executeSUBFIC(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::CMPLI:
+        executeCMPLI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::CMPI:
+        executeCMPI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ADDIC:
+        executeADDIC(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ADDIC_:
+        executeADDIC_(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ADDI:
+        executeADDI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ADDIS:
+        executeADDIS(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ORI:
+        executeORI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ORIS:
+        executeORIS(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::XORI:
+        executeXORI(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::XORIS:
+        executeXORIS(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ANDI_:
+        executeANDI_(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::ANDIS_:
+        executeANDIS_(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LWZ:
+        executeLWZ(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LWZU:
+        executeLWZU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STW:
+        executeSTW(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STWU:
+        executeSTWU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STB:
+        executeSTB(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STBU:
+        executeSTBU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LHZ:
+        executeLHZ(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LHZU:
+        executeLHZU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LHA:
+        executeLHA(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LHAU:
+        executeLHAU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STH:
+        executeSTH(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STHU:
+        executeSTHU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LMW:
+        executeLMW(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STMW:
+        executeSTMW(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LFS:
+        executeLFS(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LFSU:
+        executeLFSU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LFD:
+        executeLFD(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LFDU:
+        executeLFDU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STFS:
+        executeSTFS(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STFSU:
+        executeSTFSU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STFD:
+        executeSTFD(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::STFDU:
+        executeSTFDU(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LBZ:
+        executeLBZ(field0, field1, uimm, simm);
+        break;
+    case BroadwayDTypeInstruction::LBZU:
+        executeLBZU(field0, field1, uimm, simm);
+        break;
+    default:
+        Logger::log("Broadway", LogLevel::Error,
+                    "executeDType: Unknown D-Type instruction. CIA: " +
+                        utils::toHexString(state.cia));
+        break;
+    }
+}
+
+void Broadway::executeXType(uint32_t instruction) {
+    uint32_t op = instruction >> 26;
+    uint32_t d = (instruction >> 21) & 0x1F;
+    uint32_t a = (instruction >> 16) & 0x1F;
+    uint32_t b = (instruction >> 11) & 0x1F;
+    uint32_t xo10 = (instruction >> 1) & 0x3FF;
+    bool rc = instruction & 0x1;
+}
+
+void Broadway::executeXOType(uint32_t instruction) {
+    uint32_t d = (instruction >> 21) & 0x1F;
+}
+
 void Broadway::executeInstruction() {
     uint32_t instruction = instructionStream.read32();
+
+    state.nia = state.cia + 4;
+
+    InstructionType type = getInstructionType(instruction);
+
+    switch (type) {
+    case InstructionType::D:
+        executeDType(instruction);
+        break;
+    case InstructionType::I:
+        executeIType(instruction);
+        break;
+    case InstructionType::B:
+        executeBType(instruction);
+        break;
+    case InstructionType::SC:
+        executeSCType(instruction);
+        break;
+    case InstructionType::X:
+        executeXType(instruction);
+        break;
+    case InstructionType::XO:
+        executeXOType(instruction);
+        break;
+    case InstructionType::XL:
+        executeXLType(instruction);
+        break;
+    case InstructionType::M:
+        executeMType(instruction);
+        break;
+    case InstructionType::A:
+        executeAType(instruction);
+        break;
+    case InstructionType::PSQ_D:
+        executePSQ_DType(instruction);
+        break;
+    case InstructionType::PSQ_X:
+        executePSQ_XType(instruction);
+        break;
+    default:
+        Logger::log("Broadway", LogLevel::Error,
+                    "executeInstruction: Unknown instruction type");
+        break;
+    }
+
+    state.cia = state.nia;
 }
